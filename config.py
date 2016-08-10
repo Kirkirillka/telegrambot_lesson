b71f3daa0e5be502a620313afe5cdd3995a98bb7
Kirkirillka <kirillkukarcev@gmail.com>
2016-08-10 20:46:22 +0300

    New commit

---
+++ config.py
@@ -0,0 +1,11 @@
+
+__author__ = 'kirill'
+
+
+token="207939931:AAGDd6FjCOQ07NNXs4KByV8zNYGJLttYC08"
+knrtu_kai_token='251907412:AAGn1JEK041blsZa_lZtJTmoMzs6C-FZoUA'
+database_name='lesson2/music.db'
+shelve_name='lesson2/shelve.db'
+count_db_name='lesson2/count.db'
+
+botan_key="72f5818d-f9de-44fc-adba-7de2226f6d63"
